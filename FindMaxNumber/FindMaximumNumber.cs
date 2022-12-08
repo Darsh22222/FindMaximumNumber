3ccac3cf0832a8196e7ac0251f57c791c3514fbf
Darsh <darshankondhare2698@gmail.com>
2022-12-08 20:36:40 +0530

    [darsh] Add: Float Maximum Number

--- FindMaxNumber/FindMaximumNumber.cs
+++ FindMaxNumber/FindMaximumNumber.cs
@@ -11,6 +11,15 @@
             else
                 return c;
         }
+        public float FindMaxFloat(float a, float b, float c)
+        {
+            if (a > b && a > c)
+                return a;
+            else if (b > a && b > a)
+                return b;
+            else
+                return c;
+        }
 
     }
 }
--- FindMaxNumberTest/FindMaxTest.cs
+++ FindMaxNumberTest/FindMaxTest.cs
@@ -24,5 +24,32 @@ namespace FindMaxNumberTest
             int result = maxNum.FindMaxInteger(15, 5, 20);
             Assert.AreEqual(20, result);
         }
+        [Test]
+        public void GivenMaxFirstNumberFloat_WhenAnalysed_ShouldReturn_FirstMax()
+        {
+            float result = maxNum.FindMaxFloat(20.20f, 15.15f, 5.5f);
+            Assert.AreEqual(20.20f, result);
+        }
+        [Test]
+        public void GivenMaxSecondNumberFloat_WhenAnalysed_ShouldReturn_SecondMax()
+        {
+            float result = maxNum.FindMaxFloat(15.15f, 20.20f, 5.5f);
+            Assert.AreEqual(20.20f, result);
+        }
+        [Test]
+        public void GivenMaxThirdNumberFloat_WhenAnalysed_ShouldReturn_ThirdMax()
+        {
+            float result = maxNum.FindMaxFloat(15.15f, 5.4f, 20.20f);
+            Assert.AreEqual(20.20f, result);
+        }
     }
 }
+/*
+AAA
+Arrage - Initialization of variables
+int a = 50, b = 30, c = 20;
+Act - Creation of object and calling particular method
+int max = FindMax.MaxInt(a, b, c);
+Assest - Validating the output
+Assert.AreEqual(max, 50);
+*/
